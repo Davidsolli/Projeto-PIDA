e3c260168f65db3771d932a5b6d0fbea4c5f4ce7
Davidsolli <davidolisoa@gmail.com>
2024-09-30 15:18:36 -0300

    first commit

---
+++ src/main/java/com/david/amazonas/AmazonasApplication.java
@@ -0,0 +1,13 @@
+package com.david.amazonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonasApplication.class, args);
+	}
+
+}
