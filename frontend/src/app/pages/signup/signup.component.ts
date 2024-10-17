0bce91c12f5a72e005539435ca6df42d4d7d062c
Davidsolli <davidolisoa@gmail.com>
2024-10-16 22:47:33 -0300

    sign up layout

--- frontend/src/app/pages/signup/signup.component.ts
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -11,13 +11,15 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { Toast, ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
-  email: FormControl,
-  password: FormControl
+interface SignupForm {
+  name: FormControl;
+  email: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -25,20 +27,25 @@ interface LoginForm {
     PrimaryInputComponent,
   ],
   providers: [LoginService],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastr: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required]),
       password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [
         Validators.required,
         Validators.minLength(6),
       ]),
@@ -47,7 +54,7 @@ export class LoginComponent {
 
   submit() {
     this.loginService
-      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .login(this.signupForm.value.email, this.signupForm.value.password)
       .subscribe({
         next: () => this.toastr.success('Login feito com sucesso!'),
         error: () => this.toastr.error('Erro inesperado, tente novamente'),
@@ -55,6 +62,6 @@ export class LoginComponent {
   }
 
   navigate() {
-    this.router.navigate(['signup']);
+    this.router.navigate(['']);
   }
 }
